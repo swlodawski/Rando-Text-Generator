4b0f17cc6ab7f3f63ed4b42e5c0dc7b5308adb71
sammy wlodawski <samwlodawski@gmail.com>
2024-12-10 14:21:06 -0500

    Declared all global variables.

---
+++ script.js
@@ -0,0 +1,16 @@
+const tagOptions = [
+    "p", "h1", "h2", "h3",
+    "h4", "h5", "h6", "span",
+];
+
+const optionsContainer = document.querySelector(".options");
+const outputContainer = document.querySelector(".output");
+const tagSelect = document.getElementById("tags");
+const paragraphSlider = document.getElementById("paragraphs");
+const wordSlider = document.getElementById("words");
+const paragraphsValue = document.getElementById("paragraphsValue");
+const wordsValue = document.getElementById("wordsValue");
+
+function createOptionsUI () {
+    
+}
